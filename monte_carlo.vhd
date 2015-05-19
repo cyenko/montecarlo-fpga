bbc1b855b0d344fcba4403f650784ec60e982b0d
Christian Yenko <chris@jivehealth.com>
2015-05-19 16:05:03 -0500

    Add sort function
    From VHDL Guru
    (http://vhdlguru.blogspot.com/2010/03/vhdl-function-for-finding-square-r
    oot.html)

--- monte_carlo.vhd
+++ monte_carlo.vhd
@@ -1,7 +1,7 @@
 ----------------------------------------------------------------------------- 
 library IEEE; 
- 
 use IEEE.std_logic_1164.all; 
+use ieee.numeric_std.all;    -- for UNSIGNED
 --Additional standard or custom libraries go here 
  
 package monte_carlo is 
@@ -131,6 +131,34 @@ function log2(i: natural) return integer is
 		return ret_val;
 end function log2;
 
- 
+
+--Credit: http://vhdlguru.blogspot.com/2010/03/vhdl-function-for-finding-square-root.html
+-- Credit given to Vipin from VHDL Guru
+function  sqrt  ( d : UNSIGNED ) return UNSIGNED is
+variable a : unsigned(31 downto 0):=d;  --original input.
+variable q : unsigned(15 downto 0):=(others => '0');  --result.
+variable left,right,r : unsigned(17 downto 0):=(others => '0');  --input to adder/sub.r-remainder.
+variable i : integer:=0;
+
+begin
+for i in 0 to 15 loop
+right(0):='1';
+right(1):=r(17);
+right(17 downto 2):=q;
+left(1 downto 0):=a(31 downto 30);
+left(17 downto 2):=r(15 downto 0);
+a(31 downto 2):=a(29 downto 0);  --shifting by 2 bit.
+if ( r(17) = '1') then
+r := left + right;
+else
+r := left - right;
+end if;
+q(15 downto 1) := q(14 downto 0);
+q(0) := not r(17);
+end loop; 
+return q;
+
+end sqrt;
+
 end package body monte_carlo; 
 --------------------------------------------------
